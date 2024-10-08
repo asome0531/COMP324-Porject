696198663506c72232ee9482b1ef68fda80f6ec7
Syed12501 jamal <syedareebjamal@gmail.com>
2024-10-08 23:16:01 +0000

    Added files

---
+++ comp324.js
@@ -0,0 +1,26 @@
+
+let cart = [];
+let totalPrice = 0;
+
+function addToCart(productId) {
+    const product = document.getElementById(productId);
+    const name = product.querySelector('h3').innerText;
+    const price = parseFloat(product.querySelector('p').innerText.replace('$', ''));
+    
+    cart.push({ name, price });
+    totalPrice += price;
+    
+    displayCart();
+}
+
+function displayCart() {
+    const cartItems = document.getElementById('cartItems');
+    cartItems.innerHTML = '';
+    cart.forEach(item => {
+        const li = document.createElement('li');
+        li.innerText = `${item.name} - $${item.price.toFixed(2)}`;
+        cartItems.appendChild(li);
+    });
+
+    document.getElementById('totalPrice').innerText = totalPrice.toFixed(2);
+}
